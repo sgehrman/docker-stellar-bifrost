0d86ad8f5ff1f2146abb95d3ff9be4ac55faa23f
Steve Gehrman <sgehrman@gmail.com>
2017-12-21 01:16:49 -0800

    sf

--- entry.sh
+++ entry.sh
@@ -4,6 +4,4 @@ set -e
 
 build-config /config.json > /opt/bifrost/bifrost.cfg
 
-sleep 33333
-
-# /go/bin/bifrost server -c /opt/bifrost/bifrost.cfg
+/go/bin/bifrost server -c /opt/bifrost/bifrost.cfg
